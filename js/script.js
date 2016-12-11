6e34d6a2fb356c68289dd02f5568a491a1e04861
Rick Knight <richardknight314@hotmail.com>
2016-12-11 00:36:03 -0500

    Revised version of Project-1
    Revisions include updates to getRandomQuote function to use a do...while
    loop, and updating of the construction of the "output" variable to be
    incremental and with conditionals to check for presence of citation and
    year properties in a given quote object.  Finally, this version includes
    a set of functions which set timers for the quotes which (a) set an
    initial timer, (b) set an auto-refresh timer, and (c) reset the
    auto-refresh timer on click of button (so that you don't encounter any
    quick flashes of a quote following button click due to the original
    "auto" timer still running.  The css file needed to be revised because
    my original code upload had modified the css based on my implementation
    of dealing with missing citation and year properties. This revision
    eliminates the need to change the css; so I reset it back to it's
    original state.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // create array of quotes
 var quotes = [
     {
-        quote: "Imagination is more important than knowledge.", source: "Albert Einstein, ", citation: "Cosmic Religion, ", year: 1931, tags: "inspiration"
+        quote: "Imagination is more important than knowledge.", source: "Albert Einstein", citation: "Cosmic Religion", year: 1931, tags: "inspiration"
     },
     {
-        quote: "The act of observing disturbs the observed.", source: "Cecil Adams, ", citation: "on Heisenberg's Uncertainty Principle, ", year: 1982, tags: "facts"
+        quote: "The act of observing disturbs the observed.", source: "Cecil Adams", citation: "on Heisenberg's Uncertainty Principle", year: 1982, tags: "facts"
     },
     {
-        quote: "The first time is the best for everything. After that, you know too much and nothing's ever quite the same.", source: "Michael Caine, ", citation: "Jaws: The Revenge, ", year: 1987, tags: "inspiration"
+        quote: "The first time is the best for everything. After that, you know too much and nothing's ever quite the same.", source: "Michael Caine", citation: "Jaws: The Revenge", year: 1987, tags: "inspiration"
     },
     {
-        quote: "Of all the gin joints in all the towns in all the world, she walks into mine.", source: "Humphrey Bogart, ", citation: "Casablanca, ", year: 1942, tags: "humor"
+        quote: "Of all the gin joints in all the towns in all the world, she walks into mine.", source: "Humphrey Bogart", citation: "Casablanca", year: 1942, tags: "humor"
     },
     {
         quote: "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.", source: "Henry Ford", tags: "inspiration"
@@ -22,49 +22,55 @@ var quotes = [
         quote: "Whether you think you can, or think you can't, you're right.", source: "Henry Ford", tags: "inspiration"
     }
 ];
-
-// NOTE:  Commas were added as needed to the source and citations, and the styles.css file was edited to remove the commas before citation and year (I commented them out). This eliminated the issue of commas still appearing in cases where citation and/or year were undefined in a quote object.  
+ 
 
 var usedQuotes = [];                                        // define array to store those index values from the quotes array which have been used
 
 function getRandomQuote() {
-    var x = Math.floor(Math.random() * quotes.length);      // get randomly generated number between 0 and length of quotes array
-    if (usedQuotes.includes(x)) {                           // check if the randomly generated x has already been used (and is thus stored in the usedQuotes array)
-        getRandomQuote();                                   // if x has already been used, execute getRandomQuote() again to try another x
-    } 
-    else {
-        usedQuotes.push(x);                                 // add x to usedQuotes array since it is new (and hasn't been used previously)
-        if (usedQuotes.length === quotes.length) {          // check to see if all quotes have now been used (thus usedQuotes array length = quotes array length)
-            console.log(usedQuotes);                        // write out to the console the full usedQuotes array before clearing it
-            console.log(quotes[x]);
-            usedQuotes = [];                                // clear usedQuotes array since it's full and all quotes have now been used only once
-        } else {
-            console.log(usedQuotes);                        // if not all quotes have yet been used, log out to the console the current usedQuotes array
-            console.log(quotes[x]);
-        }
+    var x;                                                  // declare variable x to represent the current index of the quotes array
+    
+    do {
+        x = Math.floor(Math.random() * quotes.length);      // assign x a random number between 0 and length of quotes array - 1 ...
     }
+    
+    while (usedQuotes.includes(x));                         // ... if usedQuotes array already has current assignment of x, assign x a new value again
+    
+    // Once a new value of x is selected which hasn't been used already:
+    
+        usedQuotes.push(x);                                 // add x to usedQuotes array since it is new (and hasn't been used previously)
+        console.log(usedQuotes);                            // log out to the console the usedQuotes array
+        console.log(quotes[x]);                             // also log out to the console the quote object at position x in the quotes array
+        if (usedQuotes.length === quotes.length) {          // check to see if all quotes have now been used (thus usedQuotes array length = quotes array length)
+            usedQuotes = [];                                // clear usedQuotes array since it's full and all quotes have now been used only once
+        } 
+    //}
     return quotes[x];                                       // return the object located at index x of the quotes array
 }
 
 function printQuote() {
     var selectedQuote = getRandomQuote();                   // set selectedQuote variable to the returned object from the getRandomQuote function
 
-    if (!selectedQuote.citation) {                          // if citation is undefined, make it blank ("") ** SEE NOTE ABOVE REGARDING COMMAS **
-        selectedQuote.citation = "";
+    // Define variable "output" as the HTML to go into the quote-box div
+    
+    var output = '<p class="quote">' + selectedQuote.quote + '</p>';
+    output += '<p class="source">' + selectedQuote.source;              // append output with source
+    
+    if (selectedQuote.citation) {                           
+        output += '<span class="citation">' + selectedQuote.citation + '</span>';   // conditional check to see if there's a citation on current quote
     }
-    if (!selectedQuote.year) {                              // if year is undefined, make it blank ("")  ** SEE NOTE ABOVE REGARDING COMMAS **
-        selectedQuote.year = "";
+    
+    if (selectedQuote.year) {
+        output += '<span class="year">' + selectedQuote.year + '</span>';           // conditional check to see if there's a year on current quote
     }
-
-    // define variable "output" as the HTML to go into the quote-box div
-    var output = '<p class="quote">' + selectedQuote.quote + '</p><p class="source">' + selectedQuote.source + '<span class="citation">' + selectedQuote.citation + '</span><span class="year">' + selectedQuote.year + '</span></p>';
-
-
+    
+    output += '</p>';                                                               // end the output with closing paragraph tag
+    
     document.getElementById('quote-box').innerHTML = output;  // update the quote-box element's inner HTML with the "output" contents
     applyColor();                                             // call (execute) the applyColor function at the same time as printQuote is executed (see applyColor definition below)
+    restartTimer();                                           // call restartTimer so that the quote will refresh automatically after predefined seconds
 }
 
-var colors = ["#e81919", "#4ca64c", "#3232d6", "red", "blue", "purple"];               // create array of colors to be used as rotating body background colors
+var colors = ["#e81919", "#4ca64c", "#3232d6", "red", "blue", "purple"];           // create array of colors to be used as rotating body background colors
 
 function randomColor() {                                      // define function for randomly choosing one of the background colors
     var c = Math.floor(Math.random() * colors.length);
@@ -76,19 +82,19 @@ function applyColor() {                                       // define function
     document.body.style.backgroundColor = newColor;
 }
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);  // resetTimer() calls printQuote()
-// event listener to respond to "Show another quote" button clicks                  // I am attempting to reset the timer on click of button
-// when user clicks anywhere on the button, the "printQuote" function is called     // but cannot seem to get it to work after many tries
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+    // ------ set up timers to (a) automatically refresh the quote after 8 seconds and (b) reset 8-second timer on click of button ------------
+    
+var timer;  // declare variable called timer
 
-//var timer = window.setInterval(printQuote, 4000);
+function restartTimer() {
+    timer = setTimeout(printQuote, 8000);   // define function to run printQuote every 8 seconds (note that restartTimer is called by printQuote, above)
+}
 
-//function restartTimer() {
-//    return timer;
-//}
+function restartTimerWithClick () {         // define function to clear the already-running timer when button is clicked
+    clearTimeout(timer);
+}
 
-//function resetTimer() {
-//    window.clearInterval(timer);
-//    window.setInterval(printQuote, 4000);
-//    printQuote();
-//}
+setTimeout(printQuote, 8000);  // set one-time initial 8-second timer
+
+document.getElementById('loadQuote').addEventListener("click", restartTimerWithClick, false);   // cancel the already pending refresh timer
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);  // add event listener to respond to "Show another quote" button clicks
